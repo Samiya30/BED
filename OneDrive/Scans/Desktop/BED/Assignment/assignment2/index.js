bfadf8f2e5b98a6e49daab1a0ca14f09aa109a22
samiya <samiyasharma05@gmail.com>
2025-07-18 15:35:59 +0530

    done

--- OneDrive/Scans/Desktop/BED/Assignment/assignment2/index.js
+++ OneDrive/Scans/Desktop/BED/Assignment/assignment2/index.js
@@ -4,6 +4,8 @@
 // product name
 // order history
 
+
+// callback
 const fs = require("fs");
 const username = process.argv[2];
 const productname = process.argv[3];
@@ -25,7 +27,6 @@ fs.readFile("./users.txt", "utf-8", function (err, data1) {
         };
         fs.readFile("./orderHistory.txt", "utf-8", function (err, data3) {
             let orders = [];
-
             if (!err && data3.trim()) {
                 try {
                     orders = JSON.parse(data3);
@@ -41,3 +42,44 @@ fs.readFile("./users.txt", "utf-8", function (err, data1) {
         });
     });
 });
+
+
+// //using promise
+// const fs = require("fs");
+
+// function buyProduct(username, productname) {
+//     if (!username || !productname) {
+//         return console.log("data insufficient");
+//     }
+//     fs.readFile("./users.txt", "utf-8", function (err, data1) {
+//         if (err) return console.log(err);
+//         let users = data1.replace(/\r/g, '').split('\n');
+//         if (!users.includes(username)) return console.log("no user found");
+//         fs.readFile("./products.txt", "utf-8", function (err, data2) {
+//             if (err) return console.log(err);
+//             let products = data2.replace(/\r/g, '').split('\n');
+//             if (!products.includes(productname)) return console.log("no product found");
+
+//             let orderData = {
+//                 user: username,
+//                 product: productname
+//             };
+//             fs.readFile("./orderHistory.txt", "utf-8", function (err, data3) {
+//                 let orders = [];
+//                 if (!err && data3.trim()) {
+//                     try {
+//                         orders = JSON.parse(data3);
+//                     } catch (e) {
+//                         orders = [];
+//                     }
+//                 }
+//                 orders.push(orderData);
+//                 fs.writeFile("./orderHistory.txt", JSON.stringify(orders), function (err) {
+//                     if (err) return console.log(err);
+//                     console.log("order placed");
+//                 });
+//             });
+//         });
+//     });
+// }
+// buyProduct(process.argv[2], process.argv[3]);
