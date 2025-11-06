b872058e81b34e11b0bfe0879f9fc97f670ad023
samiya <samiyasharma05@gmail.com>
2025-11-06 15:45:53 +0530

    done

--- OneDrive/Scans/Desktop/BED/Lec-35/service1/index.js
+++ OneDrive/Scans/Desktop/BED/Lec-35/service1/index.js
@@ -1,7 +1,7 @@
 const {createClient}=require("redis");
 let publisher=createClient();
 
-async function notifyMe(){
+async function notifyMe(){ 
     await publisher.connect()
     await publisher.PUBLISH("notify_me","data");
     await publisher.PUBLISH("like","your post has been liked!!");
